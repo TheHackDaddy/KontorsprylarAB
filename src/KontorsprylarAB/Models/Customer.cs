b43c09894993a1051ad01a954d9bd0f88db10374
Administrator <Administrator@ACADEMY-6211VVS>
2016-11-10 11:01:55 +0100

    Lägger till lite infrastruktur för att göra i ordning orderdelen

---
+++ src/KontorsprylarAB/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KontorsprylarAB.Models
+{
+    public class Customer : User
+    {
+        public List<Product> ShoppingCart { get; set; }
+    }
+}
